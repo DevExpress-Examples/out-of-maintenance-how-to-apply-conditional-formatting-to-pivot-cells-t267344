3f5ae4464cef2ec4db0fbc21dd2efe3a9af84c9c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to apply conditional formatting to Pivot cells
+
+
+The following example demonstrates how to apply conditional formatting to Pivot cells at different detail levels.<br />- The <strong>Value</strong> format condition (<a href="https://documentation.devexpress.com/#Dashboard/clsDevExpressDashboardCommonFormatConditionValuetopic">FormatConditionValue</a>) is used to apply formatting to the first-level data cells.<br />- The <strong>Range Gradient</strong> condition (<a href="https://documentation.devexpress.com/#Dashboard/clsDevExpressDashboardCommonFormatConditionRangeGradienttopic">FormatConditionRangeGradient</a>) is used to highlight the last-level data cells.<br />- The <strong>Color Range</strong> condition (<a href="https://documentation.devexpress.com/#Dashboard/clsDevExpressDashboardCommonFormatConditionRangeSettopic">FormatConditionRangeSet</a>) is used to classify values of Grand Total cells.<br />- The <strong>Top-Bottom</strong> condition (<a href="https://documentation.devexpress.com/#Dashboard/clsDevExpressDashboardCommonFormatConditionTopBottomtopic">FormatConditionTopBottom</a>) highlights row field values corresponding to top 3 categories.
+
+<br/>
+
+
---
+++ VB/Pivot_ConditionalFormatting/Form1.vb
@@ -0,0 +1,52 @@
+﻿Imports DevExpress.XtraEditors
+Imports DevExpress.DashboardCommon
+
+Namespace Pivot_ConditionalFormatting
+    Partial Public Class Form1
+        Inherits XtraForm
+
+        Public Sub New()
+            InitializeComponent()
+            Dim dashboard As New Dashboard()
+            dashboard.LoadFromXml("..\..\Data\Dashboard.xml")
+            Dim pivot As PivotDashboardItem =
+                CType(dashboard.Items("pivotDashboardItem1"), PivotDashboardItem)
+
+            Dim firstLevelRule As New PivotItemFormatRule(pivot.Values(0))
+            Dim greaterThanCondition As New FormatConditionValue()
+            greaterThanCondition.Condition = DashboardFormatCondition.Greater
+            greaterThanCondition.Value1 = 30000
+            greaterThanCondition.StyleSettings =
+                New AppearanceSettings(FormatConditionAppearanceType.Green)
+            firstLevelRule.Condition = greaterThanCondition
+            firstLevelRule.IntersectionLevelMode = FormatConditionIntersectionLevelMode.FirstLevel
+
+            Dim lastLevelRule As New PivotItemFormatRule(pivot.Values(0))
+            Dim rangeCondition As New  _
+                FormatConditionRangeGradient(FormatConditionRangeGradientPredefinedType.WhiteGreen)
+            lastLevelRule.Condition = rangeCondition
+            lastLevelRule.IntersectionLevelMode = FormatConditionIntersectionLevelMode.LastLevel
+
+            Dim grandTotalRule As New PivotItemFormatRule(pivot.Values(0))
+            Dim rangeTotalCondition As New  _
+                FormatConditionRangeSet(FormatConditionRangeSetPredefinedType.ColorsPaleRedGreenBlue)
+            grandTotalRule.Condition = rangeTotalCondition
+            grandTotalRule.IntersectionLevelMode = FormatConditionIntersectionLevelMode.SpecificLevel
+            grandTotalRule.Level.Row = pivot.Rows(0)
+
+            Dim topCategoryRule As New PivotItemFormatRule(pivot.Values(0))
+            Dim topCondition As New FormatConditionTopBottom()
+            topCondition.TopBottom = DashboardFormatConditionTopBottomType.Top
+            topCondition.RankType = DashboardFormatConditionValueType.Number
+            topCondition.Rank = 3
+            topCondition.StyleSettings = New IconSettings(FormatConditionIconType.RatingFullGrayStar)
+            topCategoryRule.Condition = topCondition
+            topCategoryRule.IntersectionLevelMode = FormatConditionIntersectionLevelMode.SpecificLevel
+            topCategoryRule.Level.Row = pivot.Rows(0)
+            topCategoryRule.DataItemApplyTo = pivot.Rows(0)
+
+            pivot.FormatRules.AddRange(firstLevelRule, lastLevelRule, grandTotalRule, topCategoryRule)
+            dashboardViewer1.Dashboard = dashboard
+        End Sub
+    End Class
+End Namespace
