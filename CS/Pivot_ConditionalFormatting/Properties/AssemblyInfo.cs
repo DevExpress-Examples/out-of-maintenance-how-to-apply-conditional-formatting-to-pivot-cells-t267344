5d145c57bfab904f896835073ef47fbcbef7e847
BrianDX <boris.zaitsev@devexpress.com>
2019-09-13 16:13:43 +0300

    revert to 15.1

--- CS/Pivot_ConditionalFormatting/Properties/AssemblyInfo.cs
+++ CS/Pivot_ConditionalFormatting/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Pivot_ConditionalFormatting")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
